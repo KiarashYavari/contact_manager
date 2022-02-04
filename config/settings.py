ccc20da8a3b9d8a361cd324be35304c216fbab51
KiarashYavari <kiarash996@gmail.com>
2022-02-04 09:48:29 +0330

    install and set up django rest framwork

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'contact_manager.apps.ContactManagerConfig',
 ]
 
@@ -125,3 +126,8 @@ STATICFILES_DIR = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
